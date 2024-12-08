e9deb4217179d6ac653a73e084b012bc8824dd3b
Marco Antonio <marcoantonigoncas@gmail.com>
2024-12-08 02:34:46 -0600

    Cambio de nombre a clase

---
+++ File-splitters/Common/TipoParticionCombobox.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_splitters.Common
+{
+    public enum TipoParticionCombobox
+    {
+        Archivo666,
+    }
+}
