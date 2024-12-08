99e2526e2ccc61d8e9723a61c5ffe69e7c86d8d0
Marco Antonio <marcoantonigoncas@gmail.com>
2024-12-08 02:34:29 -0600

    Cambio de nombre de archivo

---
+++ File-splitters/Common/FileSizeFormatter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_splitters.Common
+{
+    public static class FileSizeFormatter
+    {
+        public static string FormatSize(long bytes)
+        {
+            int unit = 1024;
+
+
+            double bytesD = bytes;
+            string[] sufix = { "B", "KB", "MB", "GB", "TB", "PB" };
+            int i = 0;
+
+
+            // Comprobamos si los bytes son mayores que el tamaño de la unidad
+            // y si no hemos llegado al final del array de sufijos
+
+            while (bytesD >= unit && i < sufix.Length-1)
+            {
+                bytesD /= unit;
+                i++;
+            }
+
+            return string.Format("{0:0.##} {1}", bytesD, sufix[i]);
+
+        }
+    }
+}
