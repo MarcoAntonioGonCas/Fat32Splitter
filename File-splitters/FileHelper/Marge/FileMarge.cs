963a219e1452ca5491fb03ad87037689948f5334
Marco Antonio <marcoantonigoncas@gmail.com>
2024-12-08 03:29:40 -0600

    Refactorización y mejoras en la gestión de archivos

--- File-splitters/FileHelper/Marge/FileMarge.cs
+++ File-splitters/FileHelper/Marge/FileMarge.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace File_splitters.FileHelper.Marge
@@ -16,6 +17,8 @@ namespace File_splitters.FileHelper.Marge
         public event EventHandler<ProgressMargeArgs> Progreso;
         public event EventHandler<string> Error;
 
+        public bool EliminarPartes { get; set; } = false;
+
         private readonly IParticionStrategy _particion;
 
         public FileMarge(IParticionStrategy particion)
@@ -24,7 +27,7 @@ namespace File_splitters.FileHelper.Marge
         }
 
 
-        public void Marge(string nombreArchivoPrimeraParte, string directoryMarge)
+        public async Task Marge(string nombreArchivoPrimeraParte,CancellationToken cancellationToken )
         {
             // Creamos nuestro buffer para unir archivos
             byte[] buffer = new byte[FileConstants.bufferSize];
@@ -32,18 +35,19 @@ namespace File_splitters.FileHelper.Marge
             // Obtenemos la informacion del archivo
             FileInfo informacionPrimeraParte = new FileInfo(nombreArchivoPrimeraParte);
 
-            
-            string[] partes = this._particion.ObtienePartesFaltantes(informacionPrimeraParte);
 
-            long totalBytesPartes = partes.Sum(parte => new FileInfo(parte).Length);
+            string[] partes = this._particion.ObtienePartesFaltantes(informacionPrimeraParte);
+            long totalBytesPartes = ObtieneTotalBytes(nombreArchivoPrimeraParte);
+
 
 
             // Verificamos el archivoOriginal
             string soloNombreArchivoUnido = this._particion.RemueveEnumeracion(nombreArchivoPrimeraParte);
             string rutaCompletaArchivoUnido = Path.Combine(informacionPrimeraParte.Directory.FullName, soloNombreArchivoUnido);
+            bool cancelado = false;
 
+            bool existeArchivoOriginal = ExisteArchivoOriginalYCompleto(nombreArchivoPrimeraParte);
 
-            bool existeArchivoOriginal = File.Exists(rutaCompletaArchivoUnido) && new FileInfo(rutaCompletaArchivoUnido).Length == totalBytesPartes;
             if (existeArchivoOriginal)
             {
                 // No seguimos ya que existe el archivo original
@@ -54,16 +58,16 @@ namespace File_splitters.FileHelper.Marge
                     ParteActual = partes.Length,
                     TotalBytes = totalBytesPartes,
                 });
-                return;
+                
             }
 
 
             // Creamos el archivo unido
-            using (FileStream fs = new FileStream(rutaCompletaArchivoUnido, FileMode.CreateNew, FileAccess.Write))
+            using (FileStream fsDestinoMezcla = new FileStream(rutaCompletaArchivoUnido, FileMode.CreateNew, FileAccess.Write))
             {
                 long bytesEscritos = 0;
 
-                
+
                 for (int i = 0; i < partes.Length; i++)
                 {
                     string rutaParteActual = partes[i];
@@ -71,12 +75,26 @@ namespace File_splitters.FileHelper.Marge
                     using (FileStream fsParte = new FileStream(rutaParteActual, FileMode.Open, FileAccess.Read))
                     {
                         int readBytes = 0;
-                        while((readBytes = fs.Read(buffer, 0, buffer.Length)) > 0)
-                        {
+                        int countBytesParte = 0;
 
-                            fs.Write(buffer, 0, readBytes);
+                        while (countBytesParte < fsParte.Length)
+                        {
+                            readBytes = await fsParte.ReadAsync(buffer, 0, buffer.Length);
+
+
+                            if(readBytes == 0)
+                            {
+                                break;
+                            }
+
+                            await fsDestinoMezcla.WriteAsync(buffer, 0, readBytes);
+
+
+
+
 
                             bytesEscritos += readBytes;
+                            countBytesParte += readBytes;
 
 
                             this.Progreso?.Invoke(
@@ -85,11 +103,23 @@ namespace File_splitters.FileHelper.Marge
                                 {
                                     BytesActuales = bytesEscritos,
                                     TotalBytes = totalBytesPartes,
-                                    ParteActual = i+1,
+                                    ParteActual = i + 1,
                                     TotalPartes = partes.Length
                                 }
                             );
 
+
+                            if (cancellationToken.IsCancellationRequested)
+                            {
+                                cancelado = true;
+                                break;
+                            }
+
+                        }
+
+                        if (cancelado)
+                        {
+                            break;
                         }
                     }
 
@@ -99,10 +129,38 @@ namespace File_splitters.FileHelper.Marge
 
             }
 
+            if (cancelado)
+            {
+                this.Error?.Invoke(this, "Proceso cancelado");
+            }
 
 
         }
 
 
+        public long ObtieneTotalBytes(string nombreArchivoPrimeraParte)
+        {
+            FileInfo informacionPrimeraParte = new FileInfo(nombreArchivoPrimeraParte);
+            string[] partes = this._particion.ObtienePartesFaltantes(informacionPrimeraParte);
+            return partes.Sum(parte => new FileInfo(parte).Length);
+        }
+
+
+        public bool ExisteArchivoOriginalYCompleto(string nombreArchivoPrimeraParte)
+        {
+            string archivoOriginal = this._particion.RemueveEnumeracion(nombreArchivoPrimeraParte);
+
+            if(!File.Exists(archivoOriginal))
+            {
+                return false;
+            }
+
+
+            FileInfo informacionPrimeraParte = new FileInfo(nombreArchivoPrimeraParte);
+            string[] partes = this._particion.ObtienePartesFaltantes(informacionPrimeraParte);
+
+            return partes.Sum(parte => new FileInfo(parte).Length) == new FileInfo(archivoOriginal).Length;
+
+        }
     }
 }
