f522501939f16779def5d45d10ef996eb1ef6cf9
Marco Antonio <marcoantonigoncas@gmail.com>
2024-12-08 20:19:42 -0600

    Mejorado manejor de excepciones y cancelación.

--- File-splitters/FileHelper/Merge/FileMerge.cs
+++ File-splitters/FileHelper/Merge/FileMerge.cs
@@ -44,7 +44,6 @@ namespace File_splitters.FileHelper.Marge
             // Verificamos el archivoOriginal
             string soloNombreArchivoUnido = this._particion.RemueveEnumeracion(nombreArchivoPrimeraParte);
             string rutaCompletaArchivoUnido = Path.Combine(informacionPrimeraParte.Directory.FullName, soloNombreArchivoUnido);
-            bool cancelado = false;
 
             bool existeArchivoOriginal = ExisteArchivoOriginalYCompleto(nombreArchivoPrimeraParte);
 
@@ -65,76 +64,74 @@ namespace File_splitters.FileHelper.Marge
             // Creamos el archivo unido
             using (FileStream fsDestinoMezcla = new FileStream(rutaCompletaArchivoUnido, FileMode.CreateNew, FileAccess.Write))
             {
+
                 long bytesEscritos = 0;
 
-
-                for (int i = 0; i < partes.Length; i++)
+                try
                 {
-                    string rutaParteActual = partes[i];
-
-                    using (FileStream fsParte = new FileStream(rutaParteActual, FileMode.Open, FileAccess.Read))
+                    for (int i = 0; i < partes.Length; i++)
                     {
-                        int readBytes = 0;
-                        int countBytesParte = 0;
+                        string rutaParteActual = partes[i];
 
-                        while (countBytesParte < fsParte.Length)
+                        using (FileStream fsParte = new FileStream(rutaParteActual, FileMode.Open, FileAccess.Read))
                         {
-                            readBytes = await fsParte.ReadAsync(buffer, 0, buffer.Length);
+                            int readBytes = 0;
+                            int countBytesParte = 0;
 
-
-                            if(readBytes == 0)
+                            while (countBytesParte < fsParte.Length)
                             {
-                                break;
-                            }
-
-                            await fsDestinoMezcla.WriteAsync(buffer, 0, readBytes);
+                                readBytes = await fsParte.ReadAsync(buffer, 0, buffer.Length, cancellationToken);
 
 
-
-
-
-                            bytesEscritos += readBytes;
-                            countBytesParte += readBytes;
-
-
-                            this.Progreso?.Invoke(
-                                this,
-                                new ProgressMergeArgs()
+                                if (readBytes == 0)
                                 {
-                                    BytesActuales = bytesEscritos,
-                                    TotalBytes = totalBytesPartes,
-                                    ParteActual = i + 1,
-                                    TotalPartes = partes.Length
+                                    break;
                                 }
-                            );
+
+                                await fsDestinoMezcla.WriteAsync(buffer, 0, readBytes, cancellationToken);
+                                bytesEscritos += readBytes;
+                                countBytesParte += readBytes;
 
 
-                            if (cancellationToken.IsCancellationRequested)
-                            {
-                                cancelado = true;
-                                break;
+                                this.Progreso?.Invoke(
+                                    this,
+                                    new ProgressMergeArgs()
+                                    {
+                                        BytesActuales = bytesEscritos,
+                                        TotalBytes = totalBytesPartes,
+                                        ParteActual = i + 1,
+                                        TotalPartes = partes.Length
+                                    }
+                                );
+
+                                if (cancellationToken.IsCancellationRequested)
+                                {
+                                   throw new TaskCanceledException("Proceso cancelado por el usuario.");
+                                }
+
                             }
-
                         }
 
-                        if (cancelado)
-                        {
-                            break;
-                        }
                     }
+                }
+                catch (TaskCanceledException)
+                {
+                    if (File.Exists(rutaCompletaArchivoUnido))
+                    {
+                        fsDestinoMezcla.Close();
+                        File.Delete(rutaCompletaArchivoUnido);
+                    }
+
+                    this.Error?.Invoke(this, "Proceso cancelado, el archivo que estaba en proceso de union fue eliminado");
+                }
+                catch (Exception ex)
+                {
+                    this.Error?.Invoke(this, ex.Message);
 
                 }
 
 
-
             }
-
-            if (cancelado)
-            {
-                this.Error?.Invoke(this, "Proceso cancelado");
-            }
-
-
         }
 
 
@@ -145,6 +142,39 @@ namespace File_splitters.FileHelper.Marge
             return partes.Sum(parte => new FileInfo(parte).Length);
         }
 
+
+        public bool EliminarArchivoOriginal(string nombreArchivoPrimeraParte)
+        {
+            string archivoOriginal = this._particion.RemueveEnumeracion(nombreArchivoPrimeraParte);
+            if (File.Exists(archivoOriginal))
+            {
+                File.Delete(archivoOriginal);
+                return true;
+            }
+            return false;
+        }
+
+        public bool EliminarPartesDeArchivoParticionado(string nombreArchivoPrimeraParte)
+        {
+            FileInfo informacionPrimeraParte = new FileInfo(nombreArchivoPrimeraParte);
+            string[] partes = this._particion.ObtienePartesFaltantes(informacionPrimeraParte);
+            int count = 0;
+            foreach (string parte in partes)
+            {
+                if(File.Exists(parte))
+                {
+                    File.Delete(parte);
+                    count++;
+                }
+            }
+            return count == partes.Length;
+        }
+
+        public bool ExisteArchivoOriginal(string nombreArchivoPrimeraParte)
+        {
+            string archivoOriginal = this._particion.RemueveEnumeracion(nombreArchivoPrimeraParte);
+            return File.Exists(archivoOriginal);
+        }
 
         public bool ExisteArchivoOriginalYCompleto(string nombreArchivoPrimeraParte)
         {
